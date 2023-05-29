200c221c9f0f4e7d4eb9d43a3cf2b3d128f14f94
KotonBads <kotonbads@duck.com>
2023-05-29 16:48:20 +0800

    add platform checks

--- lcgo.go
+++ lcgo.go
@@ -8,16 +8,46 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"runtime"
 )
 
-func fetch() (natives map[string]any, err error) {
+func platform() (platform string, arch string) {
+	var _platform string
+	var _arch string
+
+	switch runtime.GOOS {
+	case "windows":
+		_platform = "win32"
+	case "linux":
+		_platform = "linux"
+	case "darwin":
+		_platform = "darwin"
+	default:
+		panic(fmt.Sprintf("Platform not supported: %s", runtime.GOOS))
+	}
+
+	switch runtime.GOARCH {
+	case "amd64":
+		_arch = "x64"
+	case "amd64p32":
+		if _platform != "win32" {
+			panic(fmt.Sprintf("Arch not supported for platform: %s", _platform))
+		}
+		_arch = "ia32"
+	default:
+		panic(fmt.Sprintf("Arch not supported: %s", runtime.GOARCH))
+	}
+	return _platform, _arch
+}
+
+func fetch(os string, arch string, version string) (natives map[string]any, err error) {
 	url := "https://api.lunarclientprod.com/launcher/launch"
 
 	params := map[string]string{
 		"hwid":        "0",
-		"os":          "win32",
-		"arch":        "x64",
-		"version":     "1.8.9",
+		"os":          os,
+		"arch":        arch,
+		"version":     version,
 		"branch":      "master",
 		"launch_type": "OFFLINE",
 		"classifier":  "optifine",
@@ -52,7 +82,10 @@ func write(natives map[string]any) {
 }
 
 func main() {
-	if natives, err := fetch(); err == nil {
+	version := "1.8.9"
+	os, arch := platform()
+
+	if natives, err := fetch(os, arch, version); err == nil {
 		fmt.Println(natives)
 		write(natives)
 	}
