b39b283dab25eebb71d56c52b314540b4fbbb115
KotonBads <kotonbads@duck.com>
2023-05-31 17:25:50 +0800

    cli flags

--- src/cmd/lcgo.go
+++ src/cmd/lcgo.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"fmt"
 	"lcgo"
+	"os"
 )
 
 func main() {
-	lcgo.Launch("/home/koton-bads/Documents/Go/LC/src/cmd/config.json", true)
+	config := flag.String("config", "config.json", "Path to config file")
+	debug := flag.Bool("debug", false, "Toggle debug output")
+
+	flag.Parse()
+
+	if _, err := os.Stat(*config); errors.Is(err, os.ErrNotExist) {
+		panic(fmt.Sprintf("Config does not exist: %s\nConsider running %s -h", *config, os.Args[0]))
+	}
+
+	lcgo.Launch(*config, *debug)
+
 }
